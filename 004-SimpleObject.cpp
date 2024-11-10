43707d4e2e7986f8b4fde087674d1fdd6008abb7
Roy Qu <royqh1979@gmail.com>
2024-11-10 20:33:27 +0800

    Simple Object demo

--- 004-SimpleObject.cpp
+++ 004-SimpleObject.cpp
@@ -1,5 +1,10 @@
 /**
- * 使用C语言定义javascript函数，并在js脚本中调用
+ * 创建简单的js对象：
+ * Summer = {
+ *   'initial':0,
+ *   'sum': function(...) 
+ * }
+ * 用C语言定义其普通方法
  */
 #include <quickjs.h>
 #include <stdio.h>
@@ -9,16 +14,41 @@ void fatal(const char* msg);
 void js_dump_obj(JSContext* ctx, JSValue obj);
 void js_dump_exception(JSContext* ctx);
 
-//add函数定义
-JSValue add(JSContext *ctx, JSValue this_val, int argc, JSValue* argv) {
-	if (argc!=2) {
-		JS_ThrowRangeError(ctx,"Function add() needs exact 2 arguments!");
-		return JS_EXCEPTION;
+
+/**
+ * @brief sum函数
+ * 
+ * @param ctx		js语境 
+ * @param this_val	this对象
+ * @param argc		参数的个数
+ * @param argv		参数数组
+ * 
+ * @return 
+ */
+JSValue sum(JSContext *ctx, JSValue this_val, int argc, JSValue* argv) {
+	//确保this对象合法
+	if (!JS_IsObject(this_val)) {
+		return JS_ThrowTypeError(ctx,"'this' is not an object.");
 	}
-	int32_t a,b;
-	JS_ToInt32(ctx, &a, argv[0]);
-	JS_ToInt32(ctx, &b, argv[1]);
-	return JS_NewInt32(ctx, a+b);
+	//从this对象中获取initial属性的值，保存到result变量中
+	JSValue initialVal = JS_GetPropertyStr(ctx,this_val,"initial");
+	if (JS_IsException(initialVal)) {
+		return initialVal;
+	}
+	int32_t result;
+	if (JS_ToInt32(ctx, &result, initialVal) == -1)
+		return JS_EXCEPTION;
+	
+	//将各实参的值累加到result变量上
+	for (int i=0;i<argc;i++) {
+		int32_t a;
+		if (JS_ToInt32(ctx, &a, argv[i]) == -1) 
+			return JS_EXCEPTION;
+		result+=a;
+	}
+	
+	//将result转换成JSValue后返回
+	return JS_NewInt32(ctx, result);
 }
 
 int main() {
@@ -29,15 +59,23 @@ int main() {
 	JSContext *ctx = JS_NewContext(rt);
 	if (ctx==NULL)
 		fatal("Can't create js context!");
+
+	//创建sumObj对象
+	JSValue sumObj =JS_NewObject(ctx);
+	//将0赋给sumObj的"initial"属性
+	JSValue initialVal = JS_NewInt32(ctx,0);
+	JS_SetPropertyStr(ctx, sumObj, "initial", initialVal);
 	
-	//创建add函数句柄
-	JSValue addFunc = JS_NewCFunction(ctx,add,"add",2);
-	//将add函数句柄设置成语境全局对象的add方法
+	//创建sum函数句柄并将其赋成sumObj的sum方法
+	JSValue sumFunc = JS_NewCFunction(ctx,sum,"sum function",2);
+	JS_SetPropertyStr(ctx, sumObj, "sum", sumFunc);
+	
+	//通过语境的全局对象，将sumObj赋给"Summber"变量
 	JSValue global = JS_GetGlobalObject(ctx);
-	JS_SetPropertyStr(ctx,global,"add",addFunc);
+	JS_SetPropertyStr(ctx,global,"Summer",sumObj);
 	
 	//执行js脚本
-	const char *foo_code = "result = add(3,5);";
+	const char *foo_code = "Summer.initial=10; var result = Summer.sum(1,2,3,4,5);";
 	JSValue evalResult = JS_Eval(ctx, foo_code, sizeof(foo_code), "<input>", JS_EVAL_FLAG_UNUSED);
 	//如果执行不成功，显示错误信息
 	if (JS_IsException(evalResult)) {
@@ -54,11 +92,13 @@ int main() {
 	printf("Result: %d\n",result);
 	
 	//清理
-	JS_FreeValue(ctx,jsResult);
-	JS_FreeValue(ctx,addFunc);
-	JS_FreeValue(ctx,global);
+	JS_FreeValue(ctx, jsResult);
+	JS_FreeValue(ctx, initialVal);
+	JS_FreeValue(ctx, sumFunc);
+	JS_FreeValue(ctx, global);
 	JS_FreeContext(ctx);
 	JS_FreeRuntime(rt);	
+	return 0;
 }
 
 /**
